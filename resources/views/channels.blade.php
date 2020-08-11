c07bd3ddfee2cbaa44065e19b167d576b552726e
Alan Hardman <alanaktion@gmail.com>
2020-08-10 21:53:53 -0600

    Better listing UI

--- resources/views/channels.blade.php
+++ resources/views/channels.blade.php
@@ -6,8 +6,12 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-5">
         @forelse ($channels as $channel)
             <div>
-                <a href="/user/{{ $channel->uuid }}">{{ $channel->title }}</a>
-                <div>{{ $channel->description }}</div>
+                <a href="/user/{{ $channel->uuid }}" class="text-blue-600 hover:text-blue-500">
+                    {{ $channel->title }}
+                </a>
+                <div class="text-xs text-ngray-700 h-16 overflow-hidden">
+                    {{ Str::limit($channel->description, 60) }}
+                </div>
             </div>
         @empty
             <div class="text-ngray-600 py-6">No available channels</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,12 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-5">
         @forelse ($videos as $video)
             <div>
-                <a href="/videos/{{ $video->uuid }}">{{ $video->title }}</a>
-                <div>{{ $video->description }}</div>
+                <a href="/videos/{{ $video->uuid }}" class="text-blue-600 hover:text-blue-500">
+                    {{ $video->title }}
+                </a>
+                <div class="text-xs text-ngray-700 h-16 overflow-hidden">
+                    {{ Str::limit($video->description, 60) }}
+                </div>
             </div>
         @empty
             <div class="text-ngray-600 py-6">No recent videos</div>
@@ -23,8 +27,12 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-5">
         @forelse ($channels as $channel)
             <div>
-                <a href="/user/{{ $channel->uuid }}">{{ $channel->title }}</a>
-                <div>{{ $channel->description }}</div>
+                <a href="/user/{{ $channel->uuid }}" class="text-blue-600 hover:text-blue-500">
+                    {{ $channel->title }}
+                </a>
+                <div class="text-xs text-ngray-700 h-16 overflow-hidden">
+                    {{ Str::limit($channel->description, 60) }}
+                </div>
             </div>
         @empty
             <div class="text-ngray-600 py-6">No recent channels</div>
