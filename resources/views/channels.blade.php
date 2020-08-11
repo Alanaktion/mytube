a13c2771a3ae288f9af9ff091401b3418704a971
Alan Hardman <alanaktion@gmail.com>
2020-08-10 21:57:30 -0600

    More better listing UI

--- resources/views/channels.blade.php
+++ resources/views/channels.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="container">
-    <h2 class="text-2xl">Channels</h2>
+    <h2 class="text-2xl mb-3">Channels</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-5">
         @forelse ($channels as $channel)
             <div>
-                <a href="/user/{{ $channel->uuid }}" class="text-blue-600 hover:text-blue-500">
+                <a href="/user/{{ $channel->uuid }}" class="block mb-1 text-blue-400 hover:text-blue-300">
                     {{ $channel->title }}
                 </a>
                 <div class="text-xs text-ngray-700 h-16 overflow-hidden">
--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="container">
-    <h2 class="text-2xl">Videos</h2>
+    <h2 class="text-2xl mb-3">Videos</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-5">
         @forelse ($videos as $video)
             <div>
-                <a href="/videos/{{ $video->uuid }}" class="text-blue-600 hover:text-blue-500">
+                <a href="/videos/{{ $video->uuid }}" class="block mb-1 text-blue-400 hover:text-blue-300">
                     {{ $video->title }}
                 </a>
                 <div class="text-xs text-ngray-700 h-16 overflow-hidden">
