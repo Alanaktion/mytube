36cb6db6a86ff1579011616de1f79a0164f585b3
Alan Hardman <alanaktion@gmail.com>
2020-08-12 10:42:50 -0600

    Adding symlink for video player
    No idea if this will actually work :P

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -13,4 +13,26 @@ class Video extends Model
     {
         return $this->belongsTo(Channel::class);
     }
+
+    /**
+     * Get a web-accessible symlink to the source file
+     */
+    public function link(): string
+    {
+        $ext = pathinfo($this->file_path, PATHINFO_EXTENSION);
+        $file = "{$this->uuid}.{$ext}";
+
+        // Ensure video directory exists
+        if (!is_dir(storage_path('app/public/videos'))) {
+            mkdir(storage_path('app/public/videos'), 0755, true);
+        }
+
+        // Create symlink if it doesn't exist
+        $linkPath = storage_path('app/public/videos') . DIRECTORY_SEPARATOR . $file;
+        if (!is_link($linkPath) && is_file($this->file_path)) {
+            symlink($this->file_path, $linkPath);
+        }
+
+        return "/storage/videos/$file";
+    }
 }
--- resources/views/videoShow.blade.php
+++ resources/views/videoShow.blade.php
@@ -2,14 +2,29 @@
 
 @section('content')
 <div class="container">
+    <div class="relative mb-4 lg:mb-6" style="padding-bottom: 56.25%;">
+        <video class="absolute w-full h-full" controls>
+            <source src="{{ $video->link() }}">
+            <code>{{ $video->file_path }}</code>
+        </video>
+    </div>
     <header class="mb-3 md:mb-4 lg:mb-6">
-        <h2 class="text-2xl lg:text-3xl mb-2">{{ $video->title }}</h2>
+        <div class="mb-2">
+            <h2 class="text-2xl lg:text-3xl">
+                {{ $video->title }}
+            </h2>
+            <div class="text-sm text-gray-400" title="{{ $video->published_at }}">
+                {{ $video->published_at->format('F j, Y @ g:ia') }}
+            </div>
+        </div>
         <p class="text-lg">
             <a class="text-blue-400 hover:text-blue-300" href="/channels/{{ $video->channel->uuid }}">
                 {{ $video->channel->title }}
             </a>
         </p>
     </header>
-    <code>{{ $video->file_path }}</code>
+    <div class="text-gray-400">
+        <pre class="font-sans">{{ $video->description }}</pre>
+    </div>
 </div>
 @endsection
