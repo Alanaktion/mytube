8fbfe568cb8504f96e285a1b5bf3ef59505fe61b
Alan Hardman <alanaktion@gmail.com>
2020-08-20 14:44:25 -0600

    Show search query on result page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,6 +40,7 @@ class HomeController extends Controller
         return view('search', [
             'videos' => $videos,
             'channels' => $channels,
+            'q' => $q,
         ]);
     }
 
