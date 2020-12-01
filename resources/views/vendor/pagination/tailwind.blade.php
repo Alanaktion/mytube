3b9dbd881db5314c723e0cbab126646e962b3f92
Alan Hardman <alanaktion@gmail.com>
2020-12-01 08:32:17 -0700

    Fixing light theme pagination hover

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -6,13 +6,13 @@
                     {!! __('pagination.previous') !!}
                 </span>
             @else
-                <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 dark:text-trueGray-400 dark:bg-trueGray-800 dark:border-trueGray-700 leading-5 rounded-md hover:text-trueGray-300 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:text-gray-700 dark:active:bg-trueGray-700 active:text-trueGray-400">
+                <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 dark:text-trueGray-400 dark:bg-trueGray-800 dark:border-trueGray-700 leading-5 rounded-md hover:text-blue-500 dark:hover:text-trueGray-300 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:text-gray-700 dark:active:bg-trueGray-700 active:text-trueGray-400">
                     {!! __('pagination.previous') !!}
                 </a>
             @endif
 
             @if ($paginator->hasMorePages())
-                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 dark:text-trueGray-400 dark:bg-trueGray-800 dark:border-trueGray-700 leading-5 rounded-md hover:text-trueGray-300 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:text-gray-700 dark:active:bg-trueGray-700 active:text-trueGray-400">
+                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 dark:text-trueGray-400 dark:bg-trueGray-800 dark:border-trueGray-700 leading-5 rounded-md hover:text-blue-500 dark:hover:text-trueGray-300 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:text-gray-700 dark:active:bg-trueGray-700 active:text-trueGray-400">
                     {!! __('pagination.next') !!}
                 </a>
             @else
@@ -71,7 +71,7 @@
                                         <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-blue-600 border border-blue-500 cursor-default leading-5">{{ $page }}</span>
                                     </span>
                                 @else
-                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 dark:text-trueGray-400 dark:bg-trueGray-800 dark:border-trueGray-700 leading-5 hover:text-trueGray-300 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 dark:active:bg-trueGray-700 dark:active:text-trueGray-400" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 dark:text-trueGray-400 dark:bg-trueGray-800 dark:border-trueGray-700 leading-5 hover:text-blue-500 dark:hover:text-trueGray-300 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 dark:active:bg-trueGray-700 dark:active:text-trueGray-400" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                                         {{ $page }}
                                     </a>
                                 @endif
