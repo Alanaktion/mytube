86449703eaf17a490497399fdc7e6a00919db7f9
Alan Hardman <alanaktion@gmail.com>
2020-12-01 19:25:29 -0700

    Minor style fixes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     @if (session('message'))
-    <div class="flex items-center bg-green-100 dark:bg-green-900 bg-opacity-50 dark:bg-opacity-0 border border-green-100 dark:border-green-600 p-3 rounded mb-4">
+    <div class="flex items-center bg-green-100 dark:bg-green-900 bg-opacity-50 border border-green-100 dark:border-green-600 p-3 rounded mb-4">
         <svg class="w-6 h-6 mr-2 text-green-500 dark:text-green-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
         </svg>
