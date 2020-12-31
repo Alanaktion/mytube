3a93ce7e4704e20c002753ca30a7196fd7814ccc
Alan Hardman <alanaktion@gmail.com>
2020-12-31 15:43:48 -0700

    Show channel on main video list

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -5,7 +5,7 @@
     <h2 class="text-2xl mb-3">All Videos</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 pb-5 lg:mb-6">
         @forelse ($videos as $video)
-            <x-video-link :video="$video" />
+            <x-video-link :video="$video" :showChannel="true" />
         @empty
             <div class="text-gray-400 dark:text-trueGray-600 py-6">No available videos</div>
         @endforelse
