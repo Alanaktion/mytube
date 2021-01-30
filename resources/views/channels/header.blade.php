955e8606e08be3f6dcefd41703c15d9e501668ba
Alan Hardman <alanaktion@gmail.com>
2021-01-29 21:43:02 -0700

    Adding Floatplane source link stuff

--- resources/views/channels/header.blade.php
+++ resources/views/channels/header.blade.php
@@ -3,11 +3,11 @@
         <img class="h-10 w-10 rounded-full" src="{{ $channel->image_url ?? '/images/channels/' . $channel->uuid }}" alt="{{ $channel->name }}">
         <h1 class="text-2xl lg:text-3xl ml-3">{{ $channel->title }}</h1>
         @if ($channel->source_link)
-            <a href="{{ $channel->source_link }}" class="bg-transparent text-red-600 hover:bg-red-600 hover:text-white font-bold py-2 px-4 rounded-full flex" aria-label="View on YouTube" data-tooltip>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block w-5 h-5" aria-hidden="true">
-                    <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" fill="none" stroke="currentColor"></path>
-                    <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" fill="currentColor"></polygon>
-                </svg>
+            <a href="{{ $channel->source_link }}"
+                class="bg-transparent text-red-600 hover:bg-red-600 hover:text-white font-bold py-2 px-4 rounded-full flex"
+                aria-label="View on {{ $channel->type == 'youtube' ? 'YouTube' : ucfirst($channel->type) }}"
+                data-tooltip>
+                <x-source-icon :type="$channel->type" />
             </a>
         @endif
     </div>
