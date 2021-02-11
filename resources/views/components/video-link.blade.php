21058cea75ad1ab49003c38f4f7811137358e8a4
Alan Hardman <alanaktion@gmail.com>
2021-02-10 17:57:31 -0700

    Indicate missing files

--- resources/views/components/video-link.blade.php
+++ resources/views/components/video-link.blade.php
@@ -6,6 +6,13 @@
         <span title="{{ $video->title }}">
             {{ $video->title }}
         </span>
+        @if (!$video->file_path)
+            <div class="absolute top-1 left-1 text-white" aria-label="No video file available" title="No video file available">
+                <svg class="w-6 h-6 drop-shadow" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                </svg>
+            </div>
+        @endif
         @if ($video->source_visibility != 'public')
             <div class="absolute top-1 right-1 text-white" aria-label="Visibility: {{ ucfirst($video->source_visibility) }}" title="{{ ucfirst($video->source_visibility) }}">
                 <svg class="w-5 h-5 drop-shadow" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
