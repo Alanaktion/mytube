460e8815739ab2a5c1180b394e690625cfa6a546
Alan Hardman <alanaktion@gmail.com>
2021-03-10 17:32:46 -0700

    Show published date on channel about page

--- resources/views/channels/about.blade.php
+++ resources/views/channels/about.blade.php
@@ -25,13 +25,21 @@
             </div>
             <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500 dark:text-trueGray-400">
+                    Published
+                </dt>
+                <dd class="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                    {{ $channel->published_at ? $channel->published_at->format('F j, Y') : 'Unknown' }}
+                </dd>
+            </div>
+            <div class="bg-gray-50 dark:bg-trueGray-850 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500 dark:text-trueGray-400">
                     Imported
                 </dt>
                 <dd class="mt-1 text-sm sm:mt-0 sm:col-span-2">
                     {{ $channel->created_at->format('F j, Y') }}
                 </dd>
             </div>
-            <div class="bg-gray-50 dark:bg-trueGray-850 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500 dark:text-trueGray-400">
                     Description
                 </dt>
