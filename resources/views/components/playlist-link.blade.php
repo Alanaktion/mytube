ed08cfba8e40a298a72263359d57d22003c70302
Alan Hardman <alanaktion@gmail.com>
2021-04-21 16:56:38 -0600

    Switch to line-clamp plugin

--- resources/views/components/playlist-link.blade.php
+++ resources/views/components/playlist-link.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <a href="/playlists/{{ $playlist->uuid }}" class="block line-clamp leading-snug mb-1 text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">
+    <a href="/playlists/{{ $playlist->uuid }}" class="block line-clamp-2 leading-snug mb-1 text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">
         <div class="relative pb-9/16 mb-2">
             @if ($firstVideo)
                 <img class="absolute w-full h-full object-cover" src="{{ $firstVideo->thumbnail_url ?? "/images/thumbs/{$firstVideo->uuid}" }}" alt>
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,5 +41,6 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
     ],
 };
