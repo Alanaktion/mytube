631e633438c688bdde096c2dfc895de17d78dfac
Alan Hardman <alanaktion@gmail.com>
2021-05-25 11:36:18 -0600

    Fix button border

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,8 +2,8 @@
 /** @var \Illuminate\View\ComponentAttributeBag $attributes */
 $tag = $attributes->has('href') ? 'a' : 'button';
 $classList = [
-    'inline-flex items-center justify-center font-medium border border-transparent',
-    'bg-blue-600 hover:bg-blue-500 dark:bg-blue-700 dark:hover:bg-blue-600 text-white' => $primary,
+    'inline-flex items-center justify-center font-medium border',
+    'border-transparent bg-blue-600 hover:bg-blue-500 dark:bg-blue-700 dark:hover:bg-blue-600 text-white' => $primary,
     'border-gray-300 shadow-sm text-gray-700 bg-white hover:bg-gray-50 dark:border-trueGray-600 dark:hover:border-trueGray-500 dark:bg-trueGray-700 dark:hover:bg-trueGray-600 dark:text-trueGray-100' => !$primary,
     'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-trueGray-900 dark:focus:ring-blue-600',
     'rounded-full' => $rounded,
