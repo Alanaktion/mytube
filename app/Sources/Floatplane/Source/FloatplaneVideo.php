7c71a239e1dd8d1ccb7a667bb28db6cf9d21b731
Alan Hardman <alanaktion@gmail.com>
2021-07-19 09:46:46 -0600

    Prevent Floatplane suffix invalid eager matching

--- app/Sources/Floatplane/Source/FloatplaneVideo.php
+++ app/Sources/Floatplane/Source/FloatplaneVideo.php
@@ -56,7 +56,7 @@ class FloatplaneVideo implements SourceVideo
 
     public function matchFilename(string $filename): ?string
     {
-        if (preg_match('/-([0-9A-Za-z_\-]{10})\.(mp4|m4v|avi|mkv|webm)$/i', $filename, $matches)) {
+        if (preg_match('/(?<!-)-([0-9A-Za-z_\-]{10})\.(mp4|m4v|avi|mkv|webm)$/i', $filename, $matches)) {
             return $matches[1];
         }
         return null;
