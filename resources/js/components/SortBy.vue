e11117943e71697d069c47e5d1cbdf3818a33879
Alan Hardman <alanaktion@gmail.com>
2021-11-16 21:31:49 -0700

    Gradient buttons 😊

--- resources/js/components/SortBy.vue
+++ resources/js/components/SortBy.vue
@@ -1,9 +1,7 @@
 <template>
     <Listbox v-model="value">
         <div class="relative">
-            <ListboxButton
-                class="inline-flex items-center justify-center w-full rounded-md border border-gray-300 shadow-sm px-3 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-offset-white focus:ring-blue-500 dark:border-trueGray-700 dark:bg-trueGray-800 dark:text-trueGray-300 dark:focus:ring-offset-trueGray-900"
-            >
+            <ListboxButton class="btn btn-secondary">
                 <SortDescendingIcon class="w-4 h-4 mr-1" aria-hidden="true" />
                 {{ label }}
                 <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,16 +2,13 @@
 /** @var \Illuminate\View\ComponentAttributeBag $attributes */
 $tag = $attributes->has('href') ? 'a' : 'button';
 $classList = [
-    'inline-flex items-center justify-center font-medium border py-2',
-    'border-transparent bg-blue-600 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-600 text-white' => $primary,
-    'border-gray-300 shadow-sm text-gray-700 bg-white hover:bg-gray-50 dark:border-trueGray-600 dark:hover:border-trueGray-500 dark:bg-trueGray-700 dark:hover:bg-trueGray-600 dark:text-trueGray-100' => !$primary,
-    'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-trueGray-900 dark:focus:ring-blue-600',
+    'btn',
+    'btn-primary' => $primary,
+    'btn-secondary' => !$primary,
     'rounded-full' => $rounded,
-    'rounded-md' => !$rounded,
-    'text-sm' => !$large,
-    'px-4' => !$rounded && !$large,
+    'btn-lg' => $large,
     'px-5' => $rounded && !$large,
-    'px-6' => $large,
+    'px-6' => $rounded && $large,
 ];
 @endphp
 <{{ $tag }}
