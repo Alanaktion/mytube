82644eac7f99f9f1d87e99dad4b435870dd56a19
Alan Hardman <alanaktion@gmail.com>
2022-07-07 19:19:06 -0600

    Remove redundant autoprefixer use

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,6 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss/nesting')(require('postcss-nesting')),
         require('tailwindcss'),
-        require('autoprefixer'),
     ])
     .disableSuccessNotifications();
 
