751780ca443748f6bad7ccd6ab85a6a1126efe10
Alan Hardman <alanaktion@gmail.com>
2022-07-28 21:08:16 -0600

    Correctly match older Twitter IDs
    This is probably still wrong. But it matches some stuff I have in my archives that was getting skipped before so that's good.

--- app/Sources/Twitter/Source/TwitterVideo.php
+++ app/Sources/Twitter/Source/TwitterVideo.php
@@ -59,7 +59,7 @@ class TwitterVideo implements SourceVideo
 
     public function matchUrl(string $url): ?string
     {
-        if (preg_match('/^(https?:\/\/)?(www\.)?twitter\.com\/([^\/]+)\/status\/(\d{19})/i', $url, $matches)) {
+        if (preg_match('/^(https?:\/\/)?(www\.)?twitter\.com\/([^\/]+)\/status\/(\d{18,19})/i', $url, $matches)) {
             return $matches[4];
         }
         return null;
@@ -67,15 +67,15 @@ class TwitterVideo implements SourceVideo
 
     public function matchId(string $id): bool
     {
-        return (bool)preg_match('/^\d{19}$/i', $id);
+        return (bool)preg_match('/^\d{18,19}$/i', $id);
     }
 
     public function matchFilename(string $filename): ?string
     {
-        if (preg_match('/-(\d{19})\.(mp4|m4v|avi|mkv|webm)$/i', $filename, $matches)) {
+        if (preg_match('/-(\d{18,19})\.(mp4|m4v|avi|mkv|webm)$/i', $filename, $matches)) {
             return $matches[1];
         }
-        if (preg_match('/ \[(\d{19})\]\.(mp4|m4v|avi|mkv|webm)$/i', $filename, $matches)) {
+        if (preg_match('/ \[(\d{18,19})\]\.(mp4|m4v|avi|mkv|webm)$/i', $filename, $matches)) {
             return $matches[1];
         }
         return null;
