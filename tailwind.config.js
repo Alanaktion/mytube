5f776c088a70687f5a1da83d8bde8866b41e0c20
Alan Hardman <alanaktion@gmail.com>
2022-09-21 07:13:09 -0600

    Package updates, minor CSS fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
     darkMode: 'class',
     content: [
+        './app/helpers.php',
         './resources/**/*.blade.php',
         './resources/**/*.vue',
     ],
