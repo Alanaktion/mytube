b838c2886f3cd6110f4c25266e59d7badf6a73f9
Alan Hardman <alanaktion@gmail.com>
2023-02-09 19:16:17 -0700

    Boop? 😳

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -7,9 +7,9 @@ use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
 class CheckForMaintenanceMode extends Middleware
 {
     /**
-     * The URIs that should be reachable while maintenance mode is enabled.
+     * The URIs that should be accessible while maintenance mode is enabled.
      *
-     * @var string[]
+     * @var array<int, string>
      */
     protected $except = [
         //
